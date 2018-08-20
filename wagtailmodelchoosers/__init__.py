3833e0d9c0fc3ac86da321070828c3fb32a4cc8f
Luccas Correa <luccascorrea@estudio89.com.br>
2018-08-20 19:23:20 -0300

    Created custom inline panel.

--- wagtailmodelchoosers/__init__.py
+++ wagtailmodelchoosers/__init__.py
@@ -1,5 +1,5 @@
 __title__ = 'wagtailmodelchoosers'
-__version__ = '0.4.1'
+__version__ = '0.4.2'
 __author__ = 'Springload'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2017 Springload'
--- wagtailmodelchoosers/edit_handlers.py
+++ wagtailmodelchoosers/edit_handlers.py
@@ -3,10 +3,102 @@ from wagtail.wagtailadmin.edit_handlers import BaseChooserPanel
 
 from wagtailmodelchoosers.utils import flatten, get_chooser_options
 from wagtailmodelchoosers.widgets import ModelChooserWidget, RemoteModelChooserWidget
+from wagtail.wagtailadmin.compare import M2MFieldComparison, FieldComparison, ChildRelationComparison
+from wagtail.wagtailadmin.edit_handlers import InlinePanel, BaseInlinePanel
+from wagtail.wagtailadmin import compare
+from django.utils.functional import curry
+from django.utils.html import escape
+from django.db import models
+import collections
 
+class BaseInlineModelPanel(BaseInlinePanel):
+
+    @classmethod
+    def get_comparison(cls):
+        if cls.is_relation:
+            field = cls.model._meta.get_field(cls.relation_name.replace("_set", ""))
+            field.verbose_name = cls.heading
+        else:
+            field = cls.model._meta.get_field(cls.relation_name)
+
+        field_comparisons = []
+
+        for panel in cls.get_panel_definitions():
+            field_comparisons.extend(panel.bind_to_model(cls.related.related_model).get_comparison())
+
+        return [curry(ChildModelComparison, field, field_comparisons)]
+
+class InlineModelPanel(InlinePanel):
+
+    def bind_to_model(self, model):
+        try:
+            related = getattr(model, self.relation_name).rel
+        except AttributeError:
+            related = getattr(model, self.relation_name + "_set").rel
+            related.related_name = self.relation_name + "_set"
+        return type(str('_InlinePanel'), (BaseInlineModelPanel,), {
+            'model': model,
+            'relation_name': self.relation_name,
+            'related': related,
+            'is_relation': True,
+            'panels': self.panels,
+            'heading': self.label,
+            'help_text': self.help_text,
+            # TODO: can we pick this out of the foreign key definition as an alternative?
+            # (with a bit of help from the inlineformset object, as we do for label/heading)
+            'min_num': self.min_num,
+            'max_num': self.max_num,
+            'classname': self.classname,
+        })
+
+class ChildModelComparison(ChildRelationComparison):
+
+    def __init__(self, field, field_comparisons, obj_a, obj_b):
+        self.field = field
+        self.field_comparisons = field_comparisons
+        accessor = field.get_accessor_name()
+        self.val_a = getattr(obj_a, accessor).all()
+        self.val_b = getattr(obj_b, accessor).all()
+
+class ModelComparison(M2MFieldComparison):
+    def __init__(self, field, obj_a, obj_b):
+        if isinstance(field, models.ManyToOneRel):
+            self.field = field
+            accessor = field.get_accessor_name()
+            self.val_a = getattr(obj_a, accessor).all()
+            self.val_b = getattr(obj_b, accessor).all()
+        else:
+            super().__init__(field, obj_a, obj_b)
+
+    def get_item_display(self, item):
+        if isinstance(self.val_a, collections.Iterable):
+            return str(item)
+        else:
+            return str(self.field.model.objects.get(id=item))
+
+    def get_items(self):
+        if isinstance(self.val_a, collections.Iterable):
+            return super().get_items()
+        else:
+            return self.val_a, self.val_b
+
+    def htmldiff(self):
+        # Get tags
+        items_a, items_b = self.get_items()
+        if isinstance(items_a, collections.Iterable):
+            return super().htmldiff()
+        else:
+            if items_a == items_b:
+                return escape(self.get_item_display(items_a))
+            else:
+                return compare.TextDiff([('deletion', self.get_item_display(items_a)), ('addition', self.get_item_display(items_b))]).to_html()
 
 class BaseModelChooserPanel(BaseChooserPanel):
     object_type_name = 'model'
+
+    @classmethod
+    def get_comparison_class(cls):
+        return ModelComparison
 
     @cached_classmethod
     def target_model(cls):
